1c004bf30df772c2186b304f917809c165afde94
Leytsfer <leytsfer@gmail.com>
2026-09-12 16:20:05 +0300

    Update web.lua

--- web.lua
+++ web.lua
@@ -1618,8 +1618,9 @@ writeText(allBtnX + 2, 2, allBtnText, Colors.white, State.categoryDropdownOpen a
 writeText(allBtnX + 2 + unicode.len(allBtnText), 2, "]", Colors.white, State.categoryDropdownOpen and Colors.dropdown_active or Colors.bg_button)
 table.insert(State.buttons, { id = "toggle_category", x = allBtnX, y = 2, w = UI.header.allBtnW, h = 1 })
 end
-local title = "ДОБРО ПОЖАЛОВАТЬ В МАГАЗИН TRADE MARKET SHOP!"
-local tradeMarketX = 100   -- любая колонка от 1 до 160; меньше = левее, больше = правее
+local title = "ДОБРО ПОЖАЛОВАТЬ, ВЫ НАХОДИТЕСЬ В МАГАЗИНЕ НА TRADE MARKET"
+-- центр свободной зоны справа от поля поиска:
+local tradeMarketX = math.floor((searchX + searchW + Config.SCREEN_W) / 2) - math.floor(unicode.len(title) / 2)
 writeText(tradeMarketX, 2, title, Colors.cyan, Colors.bg_header)
 local listW = math.floor(Config.SCREEN_W * UI.list.wRatio)
 Buffer.fill(1, 4, listW, 1, " ", Colors.text_bright, Colors.line)
